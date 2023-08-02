9e9d42b5ee32f614dda89ddf450e4fe58a08398e
Tyson Mathole <tmathole@astutefse.com>
2023-08-02 09:54:02 +0200

    Add project files.

---
+++ WebApplication2/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly List<User> _users = new List<User>();
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                // In a real app, you would hash the password before saving it.
+                _users.Add(user);
+
+                return RedirectToAction("Login");
+            }
+
+            return View(user);
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string emailOrUsername, string password)
+        {
+            var user = _users.FirstOrDefault(u => u.Email == emailOrUsername || u.Username == emailOrUsername);
+
+            if (user != null && user.Password == password)
+            {
+                // In a real app, you would set authentication cookies or tokens.
+                return RedirectToAction("Profile");
+            }
+
+            ViewBag.ErrorMessage = "Invalid credentials. Please try again.";
+            return View();
+        }
+
+    }
+}
---
+++ WebApplication2/Startup.cs
@@ -0,0 +1,38 @@
+﻿namespace WebApplication2
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.AspNetCore.Hosting;
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (!env.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Account}/{action=Login}/{id?}");
+            });
+        }
+    }
+
+}
